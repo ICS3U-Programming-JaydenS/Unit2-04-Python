96dbeccbd479951904004315474819ac2156dab0
Jayden Smith <jayden.smith2@stu.ocsb.ca>
2025-03-07 01:40:37 +0000

    Added area_per_circle.py

--- area_per_circle.py
+++ area_per_circle.py
@@ -10,8 +10,8 @@ def main():
     # This give the user input for the radius
     radius = int(input("What is the radius of the circle? (cm) "))
     # This part of the code calculates the Circumference and area of a circle.
-    circumference = math.pi * radius * 2
-    area = math.pi * (radius**2)
+    circumference = (math.pi * radius * 2)
+    area = (math.pi * (radius**2))
     # Code then displays information
     print("Circumference is {:.2f}cm".format(circumference))
     print("Area is {:.2f}cm".format(area))
